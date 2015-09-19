78468761aee0416849bf27c5e48832b746659bd4
Benjamin Lo <benjamin.lo246@gmail.com>
2015-09-19 06:12:20 -0400

    added dialog to add new items

--- app/src/main/java/com/savageblo/groceries/MainActivity.java
+++ app/src/main/java/com/savageblo/groceries/MainActivity.java
@@ -1,19 +1,25 @@
 package com.savageblo.groceries;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
     private GroceryAdapter groceryAdapter;
-
     private ListView groceryListView;
+    private Button button;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,21 @@ public class MainActivity extends ActionBarActivity {
         groceryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+
+            }
+        });
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        button = (Button) findViewById(R.id.addItembutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.add_grocery_dialog);
+                dialog.setTitle("Add a new item!");
+                dialog.show();
             }
         });
     }
